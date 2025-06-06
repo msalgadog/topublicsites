aeda7cba390865514599c281d1cbfff7aec6afdf
msagadog <msalgadog@live.com>
2025-06-05 19:55:07 -0600

    SwiperNews

---
+++ js/home/news.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".swiper", {
+    spaceBetween: 5,
+    autoHeight: true,
+    navigation: {
+        enabled: true,
+        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+    },
+    breakpoints: {
+        768: { slidesPerView: 2 },
+        812: {},
+        1024: { slidesPerView: 3 },
+        1280: { slidesPerView: 4 },
+        1920: { slidesPerView: 5 },
+    },
+});
