dcf8d3ffbfe59bbc6f71356b66fecd07548c9b30
msagadog <msalgadog@live.com>
2025-06-05 20:23:54 -0600

    News

--- js/home/news.js
+++ js/home/news.js
@@ -1,6 +1,5 @@
 var swiper = new Swiper(".swiper", {
     spaceBetween: 5,
-    autoHeight: true,
     navigation: {
         enabled: true,
         nextEl: ".boton-siguiente",
@@ -13,4 +12,15 @@ var swiper = new Swiper(".swiper", {
         1280: { slidesPerView: 4 },
         1920: { slidesPerView: 5 },
     },
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.dt-fecha').forEach(el => {
+        const rawDate = el.textContent.trim();
+        const dateObj = new Date(rawDate);
+        if (!isNaN(dateObj.getTime())) {
+            const opciones = { year: 'numeric', month: 'long', day: 'numeric' };
+            el.textContent = dateObj.toLocaleDateString('es-MX', opciones);
+        }
+    });
 });
